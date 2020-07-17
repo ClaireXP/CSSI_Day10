bbc4413df79e69f6459ad3d42c980fc2f711321e
Glitch (hello-webpage) <none>
2020-07-17 17:38:38 +0000

    🔮🚗 Checkpoint ./script.js:18046010/817:18576123/812 ./.glitch-assets:18046010/1620

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 /* global
-  
+*  keyCode
+*  LEFT_ARROW
+*  RIGHT_ARROW
+*  loadImage, createCanvas, background
+*  random
 */
 
 let alien;
 let rows = [];
 let xCan = 600;
 let yCan = 400;
-let ship1 = [];
-let ship2;
+let ship1, ship2;
 
 function preload(){
   alien = loadImage("https://cdn.glitch.com/fb3362c7-8e96-4501-923c-3d371f422938%2FCartoon-alien%20(1).svg?v=1595006239809");
+  ship1 = loadImage("https://cdn.glitch.com/fb3362c7-8e96-4501-923c-3d371f422938%2F219-2191732_15-flat-vector-spaceship-sprites-spaceship-sprites-png.jpg?v=1595007334346");
+  ship2 = loadImage("https://cdn.glitch.com/fb3362c7-8e96-4501-923c-3d371f422938%2Fsprite-spacecraft-pixel-art-png-favpng-j4PMyPriD0M3YygPNeW918nh6.jpg?v=1595007328133");
 }
 
 function setup(){
   createCanvas(xCan, yCan);
-  background(200);
+  background(200, 200, 200);
   
   ship1 = {
-    
+    x: xCan/4 - 50,
+    y: yCan - 75,
+    w: 50,
   }
   
   ship2 = {
-    
+    x: xCan/4 - 50,
+    y: yCan - 75,
+    w: 50,
   }
 }
 
@@ -32,47 +41,53 @@ function draw(){
 
 function keyPressed(){
   //when a or d pressed, move ship 1
+  //when LEFT_ARROW or RIGHT_ARROW pressed, move ship 2
+  if (keyCode === 'A') { 
   
+      ship1.x -= 10;
+  }
   
-  
-  
+  if (keyCode === 'D'){
+      
+      ship1.x += 10;
+    
+  }
   
   //when LEFT_ARROW or RIGHT_ARROW pressed, move ship 2
   if (keyCode === LEFT_ARROW) { 
   
-      ship1 -
+      ship2.x -= 10;
   }
   
   if (keyCode === RIGHT_ARROW){
-    
+       
+      ship2.x += 10;
     
   }
-  
-  
-  
-  
-  
-  
   
 }
 
 class enemy{
-  constructor(x, y, x1, y1, x2, y2){
+  constructor(x){
     this.x = x;
-    this.y = y;
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+    this.y = 0;
     this.w = 50;
+    this.vel = 5;
+  }
+  
+  updateY(){
+    this.y += this.vel;
   }
 }
 
 class row{
-  constructor(){  
+  constructor(offset){  
     let aliens = [];
-    for(let i=0; i<random([2, 3, 4]); i++){
-      aliens.push(new enemy(width/2));
+    let numAliens = random([2, 3, 4])
+    for(let i=1; i<=numAliens; i++){
+      aliens.push(new enemy((xCan/2)/numAliens*i + offset));
     }
+    
+    rows.push(aliens);
   }
 }
